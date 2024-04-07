67eeab5c09a559ea3750d2e3c21b72307fe643ce
MurphyBLG <vitaliy.ryavkin@gmail.com>
2024-04-07 18:55:34 +0500

    апрол

---
+++ cloud/Lab3/Lab3/Cities.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Cities.Client;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:32773");
+var client = new CitiesGame.CitiesGameClient(channel);
+using var call = client.Play();
+
+var readTask = Task.Run(async () =>
+{
+    await foreach (var response in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine(response.Message);
+    }
+});
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if (string.IsNullOrEmpty(result))
+    {
+        break;
+    }
+
+    await call.RequestStream.WriteAsync(new CitiesRequest { CityName = result });
+}
+
+Console.WriteLine("Disconnecting");
+await call.RequestStream.CompleteAsync();
+await readTask;
---
+++ cloud/Lab3/Lab3/Cities.Server/Services/CitiesService.cs
@@ -0,0 +1,189 @@
+﻿using Grpc.Core;
+
+namespace Cities.Server.Services;
+
+public class CitiesService(ILogger<CitiesService> logger) : CitiesGame.CitiesGameBase
+{
+    private List<string> usedCities = [];
+    private string lastCity = string.Empty;
+
+    public override async Task Play(IAsyncStreamReader<CitiesRequest> requestStream, IServerStreamWriter<CitiesReply> responseStream, ServerCallContext context)
+    {
+        try
+        {
+            await responseStream.WriteAsync(new CitiesReply { Message = "Добро пожаловать в игру Города!" });
+            await responseStream.WriteAsync(new CitiesReply { Message = "Давайте начнем!" });
+            await responseStream.WriteAsync(new CitiesReply { Message = "Введите название города (или 'exit' для выхода): " });
+
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                var city = message.CityName.ToLower();
+
+                if (string.IsNullOrEmpty(city))
+                {
+                    await responseStream.WriteAsync(new CitiesReply { Message = "Игра окончена" });
+                    return;
+                }
+
+                if (city.ToLower() == "exit")
+                {
+                    await responseStream.WriteAsync(new CitiesReply { Message = "Спасибо за игру! До свидания!" });
+                    return;
+                }
+
+                if (usedCities.Contains(city))
+                {
+                    await responseStream.WriteAsync(new CitiesReply { Message = "Этот город уже был использован. Попробуйте другой." });
+                    continue;
+                }
+
+                if (lastCity != string.Empty && !IsCityValid(city, lastCity))
+                {
+                    await responseStream.WriteAsync(new CitiesReply { Message = "Название города некорректно. Попробуйте другой." });
+                    continue;
+                }
+
+                usedCities.Add(city);
+                lastCity = city;
+
+                await responseStream.WriteAsync(new CitiesReply { Message = $"Отлично! Ваш город: {city}" });
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+        }
+    }
+
+    private void TimerEllapsed(object? state) => throw new Exception("connection aborted with timeout");
+
+    bool IsCityValid(string city, string lastCity)
+    {
+        var allCities = GetListOfAllCities();
+        if (!allCities.Select(c => c.ToLower()).Contains(city.ToLower()))
+        {
+            return false;
+        }
+
+        if (usedCities.Select(c => c.ToLower()).Contains(city))
+        {
+            return false;
+        }
+
+        var lastChar = char.ToLower(lastCity[lastCity.Length - 1]);
+        var firstChar = char.ToLower(city[0]);
+
+        return !(firstChar != lastChar && !(lastChar == 'ь' && firstChar == lastCity[lastCity.Length - 2]));
+    }
+
+    static List<string> GetListOfAllCities()
+    {
+        return [
+            "Москва",
+            "Санкт-Петербург",
+            "Нью-Йорк",
+            "Лондон",
+            "Париж",
+            "Берлин",
+            "Токио",
+            "Пекин",
+            "Рим",
+            "Мадрид",
+            "Вашингтон",
+            "Осло",
+            "Стокгольм",
+            "Афины",
+            "Каир",
+            "Дели",
+            "Бангкок",
+            "Мехико",
+            "Сидней",
+            "Канберра",
+            "Оттава",
+            "Монреаль",
+            "Торонто",
+            "Калининград",
+            "Варшава",
+            "Будапешт",
+            "Прага",
+            "Вена",
+            "Брюссель",
+            "Амстердам",
+            "Хельсинки",
+            "Рейкьявик",
+            "Осака",
+            "Сеул",
+            "Пхеньян",
+            "Пхеньян",
+            "Стамбул",
+            "Анкара",
+            "Караганда",
+            "Алматы",
+            "Астана",
+            "Ашхабад",
+            "Душанбе",
+            "Бишкек",
+            "Ташкент",
+            "Дакка",
+            "Катманду",
+            "Сингапур",
+            "Джакарта",
+            "Бейрут",
+            "Дамаск",
+            "Абу-Даби",
+            "Доха",
+            "Мускат",
+            "Кувейт",
+            "Манама",
+            "Рияд",
+            "Сана",
+            "Тегеран",
+            "Багдад",
+            "Дамаск",
+            "Бейрут",
+            "Амман",
+            "Иерусалим",
+            "Джерусалим",
+            "Киев",
+            "Минск",
+            "Рига",
+            "Таллин",
+            "Вильнюс",
+            "Любляна",
+            "Загреб",
+            "Скопье",
+            "София",
+            "Белград",
+            "Тирана",
+            "Подгорица",
+            "Приштина",
+            "Сараево",
+            "Тирана",
+            "Тирасполь",
+            "Чисинау",
+            "Кишинев",
+            "Тбилиси",
+            "Ереван",
+            "Баку",
+            "Ереван",
+            "Анкара",
+            "Измир",
+            "Анталья",
+            "Хельсинки",
+            "Копенгаген",
+            "Осло",
+            "Стокгольм",
+            "Рейкьявик",
+            "Кейптаун",
+            "Йоханнесбург",
+            "Претория",
+            "Дурбан",
+            "Лагос",
+            "Киншаса",
+            "Найроби",
+            "Каир",
+            "Александрия",
+            "Константинополь"
+        ];
+    }
+}
